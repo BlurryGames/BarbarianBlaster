62e3b74e8b0620ec262e056e6e1ce6abc5fa3ef2
BlurryGames <julian.lb.games@gmail.com>
2024-05-31 19:57:09 +0200

    Section3: Barbarian Blaster / Lesson12: Updating Gridmap Cells

--- ray_picker_camera/ray_picker_camera.gd
+++ ray_picker_camera/ray_picker_camera.gd
@@ -1,9 +1,18 @@
 class_name RayPickerCamera extends Camera3D
 
+@export var grid_map: GridMap = null
+
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var mouse_position: Vector2 = get_viewport().get_mouse_position()
 	ray_cast_3d.target_position = project_local_ray_normal(mouse_position) * 100.0
 	ray_cast_3d.force_raycast_update()
-	printt(ray_cast_3d.get_collider(), ray_cast_3d.get_collision_point())
+	
+	if ray_cast_3d.is_colliding():
+		var collider: Object = ray_cast_3d.get_collider()
+		if collider is GridMap:
+			var collision_point: Vector3 = ray_cast_3d.get_collision_point()
+			var cell: Vector3i = grid_map.local_to_map(collision_point)
+			if grid_map.get_cell_item(cell) == 0:
+				grid_map.set_cell_item(cell, 1)
