eb833c7ea364a8d2d60c9e7810480c33615413ec
BlurryGames <julian.lb.games@gmail.com>
2024-06-01 12:41:37 +0200

    Section3: Barbarian Blaster / Lesson19: Damaging Enemies

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -1,8 +1,18 @@
 class_name Enemy extends PathFollow3D
 
 @export var speed: float = 5.0
+@export var max_health: int = 50
+
+var current_health: int = 0:
+	set(health_in):
+		current_health = health_in
+		if current_health < 1:
+			queue_free()
 
 @onready var base: Base = get_tree().get_first_node_in_group("Base")
+
+func _ready() -> void:
+	current_health = max_health
 
 func _process(delta: float) -> void:
 	progress += speed * delta
