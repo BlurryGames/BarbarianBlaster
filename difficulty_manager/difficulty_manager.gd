c5f4f770b0f609d6e7bed6b9f0a297c4d4605e90
BlurryGames <julian.lb.games@gmail.com>
2024-06-01 16:27:04 +0200

    Section3: Barbarian Blaster / Lesson26: Scalling Enemy Health

--- difficulty_manager/difficulty_manager.gd
+++ difficulty_manager/difficulty_manager.gd
@@ -1,6 +1,10 @@
 class_name DifficultyManager extends Node
 
+signal stop_spawning_enemies
+
 @export var spawn_time_curve: Curve = null
+@export var enemy_health_curve: Curve = null
+
 @export var game_lenght: float = 30.0
 
 @onready var timer: Timer = $Timer
@@ -8,11 +12,14 @@ class_name DifficultyManager extends Node
 func _ready() -> void:
 	timer.start(game_lenght)
 
-func _process(_delta: float) -> void:
-	print(get_spawn_time())
-
 func game_progress_ratio() -> float:
 	return 1.0 - (timer.time_left / game_lenght)
 
 func get_spawn_time() -> float:
 	return spawn_time_curve.sample(game_progress_ratio())
+
+func get_enemy_health() -> float:
+	return enemy_health_curve.sample(game_progress_ratio())
+
+func _on_timer_timeout() -> void:
+	stop_spawning_enemies.emit()
