e6e243486b6290b7760dff9f58ce2f36c03ef1a1
BlurryGames <julian.lb.games@gmail.com>
2024-06-01 11:52:49 +0200

    Section3: Barbarian Blaster / Lesson17: Introducing Timers

--- turret/turret.gd
+++ turret/turret.gd
@@ -2,6 +2,9 @@ class_name Turret extends Node3D
 
 @export var projectile: PackedScene = null
 
-func _ready() -> void:
+@onready var turret_top: MeshInstance3D = $TurretBase/TurretTop
+
+func _on_timer_timeout() -> void:
 	var shot: Projectile = projectile.instantiate()
 	add_child(shot)
+	shot.global_position = turret_top.global_position
