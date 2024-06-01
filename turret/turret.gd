5615ada1b763302db69783c0c35ec6182ed4ed0c
BlurryGames <julian.lb.games@gmail.com>
2024-06-01 12:20:41 +0200

    Section3: Barbarian Blaster / Lesson18: Aiming The Turrets

--- turret/turret.gd
+++ turret/turret.gd
@@ -2,9 +2,16 @@ class_name Turret extends Node3D
 
 @export var projectile: PackedScene = null
 
+var enemy_path: Path3D = null
+
 @onready var turret_top: MeshInstance3D = $TurretBase/TurretTop
+
+func _physics_process(delta: float) -> void:
+	var enemy: Enemy = enemy_path.get_children().back()
+	look_at(enemy.global_position, Vector3.UP, true)
 
 func _on_timer_timeout() -> void:
 	var shot: Projectile = projectile.instantiate()
 	add_child(shot)
 	shot.global_position = turret_top.global_position
+	shot.direction = global_transform.basis.z
